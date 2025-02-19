f4e8d50b34c364e526d43c02fa13df7ff9bdfd61
Imam Baehaqi <haqisaurus@gmail.com>
2025-02-19 13:02:41 +0700

    change fiber version

--- api/index.go
+++ api/index.go
@@ -1,21 +1,41 @@
 package handler
 
 import (
-    "log"
-
-    "github.com/gofiber/fiber/v3"
+ "github.com/gofiber/fiber/v2/middleware/adaptor"
+ "github.com/gofiber/fiber/v2"
+ "net/http"
 )
 
-func main() {
-    // Initialize a new Fiber app
-    app := fiber.New()
+// Handler is the main entry point of the application. Think of it like the main() method
+func Handler(w http.ResponseWriter, r *http.Request) {
+ // This is needed to set the proper request path in `*fiber.Ctx`
+ r.RequestURI = r.URL.String()
 
-    // Define a route for the GET method on the root path '/'
-    app.Get("/", func(c fiber.Ctx) error {
-        // Send a string response to the client
-        return c.SendString("Hello, World 👋!")
-    })
+ handler().ServeHTTP(w, r)
+}
 
-    // Start the server on port 3000
-    log.Fatal(app.Listen(":8000"))
+// building the fiber application
+func handler() http.HandlerFunc {
+ app := fiber.New()
+
+ app.Get("/v1", func(ctx *fiber.Ctx) error {
+  return ctx.JSON(fiber.Map{
+   "version": "v1",
+  })
+ })
+
+ app.Get("/v2", func(ctx *fiber.Ctx) error {
+  return ctx.JSON(fiber.Map{
+   "version": "v2",
+  })
+ })
+
+ app.Get("/", func(ctx *fiber.Ctx) error {
+  return ctx.JSON(fiber.Map{
+   "uri":  ctx.Request().URI().String(),
+   "path": ctx.Path(),
+  })
+ })
+
+ return adaptor.FiberApp(app)
 }
